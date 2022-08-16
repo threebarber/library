f350a2496ee7f5c86ddbed7c7688d936ec2ad500
threebarber <dvtdropshot@gmail.com>
2022-08-16 13:44:43 -0400

    added new book form, formatting updates, function to add books

--- scripts.js
+++ scripts.js
@@ -2,10 +2,15 @@ let bookLibrary = [];
 
 let idCounter = 1;
 
+document.querySelector("#addButton").addEventListener('click',function(){
+    addNewBook();
+})
+
 
 const testBook1 = new Book(idCounter,"edward abbey","monkey gang",384,false);
 idCounter++;
 const testBook2 = new Book(idCounter,"edward jones","monkey team",323,true);
+idCounter++;
 
 bookLibrary.push(testBook1);
 bookLibrary.push(testBook2);
@@ -30,6 +35,28 @@ function Book(id,author,title,pages,read){
 }
 
 
+function addNewBook(){
+    let author = document.querySelector("#authorInput").value;
+    let title = document.querySelector("#titleInput").value;
+    let pages = document.querySelector("#pagesInput").value;
+
+    var read;
+    
+    if(document.querySelector("#readCheckbox").checked){
+        read = true;
+    }else{
+        read = false;
+    }
+
+    var newBook = new Book(idCounter,author,title,pages,read);
+
+    displaySingleBook(newBook);
+
+    bookLibrary.push(newBook);
+    console.log(bookLibrary);
+}
+
+
 
 function displayBooks(bookLibrary){
 
@@ -42,15 +69,50 @@ function displayBooks(bookLibrary){
         var newBook = document.createElement("div");
         newBook.classList.add("bookItem");
     
-        var newBookText = document.createElement("p");
-        newBookText.innerText = `Title: ${book.title} Author: ${book.author}`;
+        var newBookTitle = document.createElement("h3");
+        newBookTitle.innerText = `Title: ${book.title}`;
 
-        newBook.appendChild(newBookText);
+        var newBookAuthor = document.createElement("p");
+        newBookAuthor.innerText = `Author: ${book.author}`
+
+
+        var newBookpagesRead = document.createElement("p");
+        newBookpagesRead.innerText = `Pages: ${book.pages} Read? ${book.read}`;
+
+
+        newBook.appendChild(newBookTitle);
+        newBook.appendChild(newBookAuthor);
+        newBook.appendChild(newBookpagesRead);
 
         bookDiv.appendChild(newBook);
 
 
     }
+}
+
+
+function displaySingleBook(book){
+
+    const bookDiv = document.querySelector(".bookDiv");
+
+    
+    var newBook = document.createElement("div");
+    newBook.classList.add("bookItem");
+
+    var newBookTitle = document.createElement("h3");
+    newBookTitle.innerText = `Title: ${book.title}`;
+
+    var newBookAuthor = document.createElement("p");
+    newBookAuthor.innerText = `Author: ${book.author}`
+
+
+    var newBookpagesRead = document.createElement("p");
+    newBookpagesRead.innerText = `Pages: ${book.pages} Read? ${book.read}`;
+
+
+    newBook.appendChild(newBookTitle);
+    newBook.appendChild(newBookAuthor);
+    newBook.appendChild(newBookpagesRead);
 }
 
 
